96348a79fa0a5f2bfe21a0bdf9436e746bbe294f
Jonatan <Jonatan.holmgren@hotmail.com>
2018-09-23 12:46:16 -0400

    First batch Unity

---
+++ Assets/MyScripts/BreakMe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakMe : MonoBehaviour {
+    public GameObject window1;
+    // Use this for initialization
+    void Start () {
+        window1 = GameObject.Find("Window");
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player") // this string is your newly created tag
+        {
+
+            window1.GetComponent<BreakableWindow>().breakWindow();
+            
+        }
+    }
+}
